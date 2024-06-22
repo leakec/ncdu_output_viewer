94b09e89ba779bb9a5ce2ece2a7214f672a8139a
leakec <leakec57@gmail.com>
2024-06-22 16:28:30 -0700

    Adding stuff from other laptop for testing.

--- post_test.sql
+++ post_test.sql
@@ -40,3 +40,63 @@ LANGUAGE plpgsql;
 SELECT m.*, get_row_by_id(array[1,2]) as children
 FROM db m 
 WHERE m.id = 0
+
+
+-- Stuff from other computer
+, (1, 0, '{3,4}'), (2,0,'{}'), (3, 1, '{}'), (4, 1, '{}')
+
+CREATE TABLE db
+(
+  id int,
+  parent int,
+  child  int[]
+);
+
+INSERT INTO db(id, parent, child)
+VALUES (0, 0, '{1, 2}'), (1, 0, '{3,4}'), (2,0,'{}'), (3, 1, '{}'), (4, 1, '{}')
+
+
+
+
+with children as (
+  SELECT t.* 
+  FROM db t
+  WHERE t.parent = 1
+  ),
+json_children as (
+  SELECT row_to_json(children)
+  FROM children
+  )
+SELECT m.*, 3 as children
+FROM db m
+WHERE m.id = 1;
+
+
+
+SELECT m.id, jsonb_agg(c) as children
+FROM db m
+JOIN db c ON c.id = ANY(m.child)
+WHERE m.id = 1
+GROUP BY m.id
+
+
+
+
+WITH RECURSIVE node_tree AS (
+  SELECT *, 1 AS level
+  FROM db
+  WHERE id = 0
+  
+  UNION ALL
+                                                       
+  SELECT c.*, p.level + 1 AS level
+  FROM db c
+  JOIN node_tree p ON c.id = ANY(p.child)
+   WHERE p.level < 2
+)
+SELECT m.id, jsonb_agg(c) as children
+FROM node_tree m
+JOIN node_tree c ON c.id = ANY(m.child)
+WHERE m.id = 0
+GROUP BY m.id
+
