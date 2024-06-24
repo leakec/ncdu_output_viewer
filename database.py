40b62e3482ba2a5f313d5daefef35349be4ac7d9
leakc57@gmail.com <carl.leake@jpl.nasa.gov>
2024-06-23 17:57:41 -0700

    Removing these since they are in public now.
    Removing since it is in public.

--- database.py
+++ database.py
@@ -19,7 +19,7 @@ cursor = conn.cursor()
 ID = 0
 def recurseData(node,parent_id=0):
     """Recurse through the data structure. Do the following:
-    * Add an ID field to each object.
+    * Add an id field to each object.
     * Sum up dsize and asize based on children.
     * Add each node to the postgres database."""
     global ID
@@ -27,23 +27,23 @@ def recurseData(node,parent_id=0):
     # Set default values for this node
     node.setdefault("dsize", 0)
     node.setdefault("asize", 0)
-    node["ID"] = ID
-    node["parent_ID"] = parent_id
-    node["child_IDs"] = []
+    node["id"] = ID
+    node["parent_id"] = parent_id
+    node["child_ids"] = []
     ID += 1
 
     # Move through all children first. This ensures
     # the children will have an ID and dsize.
     for child in node.get("children", []):
-        recurseData(child, parent_id=node["ID"])
+        recurseData(child, parent_id=node["id"])
         node["asize"] += child["asize"]
         node["dsize"] += child["dsize"]
-        node["child_IDs"].appent(child["ID"])
+        node["child_ids"].append(child["id"])
 
     # Insert DataFrame to PostgreSQL
     cursor.execute(
-        "INSERT INTO db (ID, name, dsize, asize, parent_ID, child_IDs) VALUES (%s, %s, %s, %s)",
-        (node["ID"], node["name"], node["dsize"], node["asize"], node["parent_ID"], node["child_IDs"])
+        "INSERT INTO db (id, name, dsize, asize, parent_id, child_ids) VALUES (%s, %s, %s, %s, %s, %s)",
+        (node["id"], node["name"], node["dsize"], node["asize"], node["parent_id"], node["child_ids"])
     )
 
 recurseData(data)
