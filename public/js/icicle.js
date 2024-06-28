dfce54b47bfbac60e4e260c407664b986d8e9f1e
leakc57@gmail.com <carl.leake@jpl.nasa.gov>
2024-06-28 10:19:15 -0700

    Making some progress. At least we can transition to different spots. The transitions look weird and sometimes lables don't show up, but it's partially working.

--- public/js/icicle.js
+++ public/js/icicle.js
@@ -1,34 +1,21 @@
 import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
 // import { data } from "./data.js";
 
-async function getData(nodeId) {
-    return fetch(`http://localhost:3000/node/${nodeId}`).then(x=>x.json());
+async function getData(nodeId, length) {
+    return fetch(`http://localhost:3000/node/${nodeId}/${length}`).then(x=>x.json()).then(data=>data['0']['get_child_as_json']);
 }
 
 export async function icicle() {
     //let dark = await getData(0);
     //console.log(dark.json());
     //let data = await d3.json(dark);
-    let data = (await getData(0))['0']['get_child_as_json'];
-    console.log(data);
+    // let data = await getData(0,2);
+    // console.log(data);
 
     // Specify the chart’s dimensions.
     const width = 928;
     const height = 1200;
-
-    // Create the color scale.
-    const color = d3.scaleOrdinal(
-        d3.quantize(d3.interpolateRainbow, data.children.length + 1),
-    );
-
-    // Compute the layout.
-    const hierarchy = d3
-        .hierarchy(data)
-        .sum((d) => d.dsize)
-        .sort((a, b) => b.height - a.height || b.dsize - a.dsize);
-    const root = d3
-        .partition()
-        .size([height, ((hierarchy.height + 1) * width) / 3])(hierarchy);
+    const num_layers = 3;
 
     // Create the SVG container.
     const svg = d3
@@ -38,14 +25,37 @@ export async function icicle() {
         .attr("height", height)
         .attr("style", "max-width: 100%; height: auto; font: 10px sans-serif;");
 
+    let data = await getData(0, num_layers-1);
+    // Compute the layout.
+    let hierarchy = d3
+        .hierarchy(data)
+        .sum((d) => d.dsize)
+        .sort((a, b) => b.height - a.height || b.dsize - a.dsize);
+
+    let root = d3
+        .partition()
+        .size([height, ((hierarchy.height + 1) * width) / num_layers])(hierarchy);
+
+    const color = d3.scaleOrdinal(
+        d3.quantize(d3.interpolateRainbow, data.children.length + 1),
+    );
+
     // Append cells.
-    const cell = svg
+    let cell = svg
         .selectAll("g")
         .data(root.descendants())
         .join("g")
         .attr("transform", (d) => `translate(${d.y0},${d.x0})`);
 
-    const rect = cell
+    let text = cell
+        .append("text")
+        .style("user-select", "none")
+        .attr("pointer-events", "none")
+        .attr("x", 4)
+        .attr("y", 13)
+        .attr("fill-opacity", (d) => +labelVisible(d));
+
+    let rect = cell
         .append("rect")
         .attr("width", (d) => d.y1 - d.y0 - 1)
         .attr("height", (d) => rectHeight(d))
@@ -58,18 +68,10 @@ export async function icicle() {
         .style("cursor", "pointer")
         .on("click", clicked);
 
-    const text = cell
-        .append("text")
-        .style("user-select", "none")
-        .attr("pointer-events", "none")
-        .attr("x", 4)
-        .attr("y", 13)
-        .attr("fill-opacity", (d) => +labelVisible(d));
-
     text.append("tspan").text((d) => d.data.name);
 
-    const format = d3.format(",d");
-    const tspan = text
+    let format = d3.format(",d");
+    let tspan = text
         .append("tspan")
         .attr("fill-opacity", (d) => labelVisible(d) * 0.7)
         .text((d) => ` ${format(d.dsize)}`);
@@ -83,34 +85,98 @@ export async function icicle() {
                 .join("/")}\n${format(d.dsize)}`,
     );
 
-    // On click, change the focus and transitions it into view.
     let focus = root;
+
     function clicked(event, p) {
+        if (focus.data.id == p.data.id)
+        {
+            p = p.parent;
+        }
         focus = focus === p ? (p = p.parent) : p;
 
-        root.each(
-            (d) =>
-                (d.target = {
-                    x0: ((d.x0 - p.x0) / (p.x1 - p.x0)) * height,
-                    x1: ((d.x1 - p.x0) / (p.x1 - p.x0)) * height,
-                    y0: d.y0 - p.y0,
-                    y1: d.y1 - p.y0,
-                }),
-        );
+        getData(p.data.id, num_layers-1).then(new_data => {
 
-        const t = cell
-            .transition()
-            .duration(750)
-            .attr(
-                "transform",
-                (d) => `translate(${d.target.y0},${d.target.x0})`,
+            // Append new data to the old data structure
+            let dark = p;
+            const p_ids = []
+            while (dark.data.id != 0)
+            {
+                p_ids.push(dark.data.id);
+                dark = dark.parent;
+            }
+
+            let x = data;
+            p_ids.slice(1).reverse().forEach(p_id => {
+                let dark = x.child_ids.findIndex((element) => element == p_id);
+                x = x.children[dark];
+            });
+
+            let dark2 = x.child_ids.findIndex((element) => element == p_ids[0]);
+            x.children[dark2] = new_data;
+
+            // Recompute the hierarchy and partition with the new data structure
+            hierarchy = d3
+                .hierarchy(data)
+                .sum((d) => d.dsize)
+                .sort((a, b) => b.height - a.height || b.dsize - a.dsize);
+
+            root = d3
+                .partition()
+                .size([height, ((hierarchy.height + 1) * width) / num_layers])(hierarchy);
+
+            root.each(
+                (d) =>
+                    (d.target = {
+                        x0: ((d.x0 - p.x0) / (p.x1 - p.x0)) * height,
+                        x1: ((d.x1 - p.x0) / (p.x1 - p.x0)) * height,
+                        y0: d.y0 - p.y0,
+                        y1: d.y1 - p.y0,
+                    }),
             );
 
-        rect.transition(t).attr("height", (d) => rectHeight(d.target));
-        text.transition(t).attr("fill-opacity", (d) => +labelVisible(d.target));
-        tspan
-            .transition(t)
-            .attr("fill-opacity", (d) => labelVisible(d.target) * 0.7);
+            svg.selectAll("g").remove();
+
+            cell = svg
+                .selectAll("g")
+                .data(root.descendants())
+                .join("g")
+                .attr("transform", (d) => `translate(${d.y0},${d.x0})`);
+
+            text = cell
+                .append("text")
+                .style("user-select", "none")
+                .attr("pointer-events", "none")
+                .attr("x", 4)
+                .attr("y", 13)
+                .attr("fill-opacity", (d) => +labelVisible(d));
+
+            rect = cell
+                .append("rect")
+                .attr("width", (d) => d.y1 - d.y0 - 1)
+                .attr("height", (d) => rectHeight(d))
+                .attr("fill-opacity", 0.6)
+                .attr("fill", (d) => {
+                    if (!d.depth) return "#ccc";
+                    while (d.depth > 1) d = d.parent;
+                    return color(d.data.name);
+                })
+                .style("cursor", "pointer")
+                .on("click", clicked);
+
+            text.append("tspan").text((d) => d.data.name);
+
+            const t = cell
+                .transition()
+                .duration(750)
+                .attr(
+                    "transform",
+                    (d) => `translate(${d.target.y0},${d.target.x0})`,
+                );
+
+            rect.transition(t).attr("height", (d) => rectHeight(d.target));
+            text.transition(t).attr("fill-opacity", (d) => +labelVisible(d.target));
+            tspan.transition(t).attr("fill-opacity", (d) => labelVisible(d.target) * 0.7);
+        });
     }
 
     function rectHeight(d) {
