b1a3ae589aa4f14e94efb571d29f92aac23307f3
leakec <leakec57@gmail.com>
2024-06-29 07:38:19 -0700

    Moving function to postgres file so it gets added once rather than during each query.

--- database.sql
+++ database.sql
@@ -1,9 +1,4 @@
--- Connect to PostgreSQL using psql or any PostgreSQL client
--- CREATE DATABASE database;
--- -- -- \copy database from program 'sed -e ''s/\\/\\\\/g'' /home/leake/Downloads/ncdu_output_viewer/new.json';
--- -- 
--- \c database  -- Connect to the newly created database
-
+-- Create table for the database entries
 CREATE TABLE db (
     ID INT PRIMARY KEY,
     name VARCHAR(100),
@@ -12,3 +7,39 @@ CREATE TABLE db (
     parent_ID INT,
     child_IDs INT[]
 );
+
+-- Create function to query data
+CREATE OR REPLACE FUNCTION get_child_as_json(input_id INT, level INT, max_level INT)
+RETURNS jsonb as 
+$$
+DECLARE
+	result jsonb;
+BEGIN
+   IF level < max_level THEN
+     -- Aggregate the IDs that match the input array
+     SELECT to_jsonb(rows)
+     INTO result
+     FROM 
+     (
+      SELECT m.*, (
+        SELECT jsonb_agg(get_child_as_json(child, level+1, max_level))
+         FROM UNNEST(m.child_ids) AS child
+      ) AS children
+       FROM db m
+       WHERE m.id = input_id
+     ) AS rows;
+  ELSE
+    SELECT to_jsonb(rows)
+    INTO result
+    FROM (
+      SELECT m.*, array[]::integer[] AS children
+      FROM db m
+      WHERE m.id = input_id
+    ) as rows;
+  END IF;
+
+  RETURN result;
+END;
+$$
+LANGUAGE plpgsql;
+
