6ab0b3c03f02d67080a95cb49da53b0af75c2ff8
leakec <leakec57@gmail.com>
2024-06-29 07:03:58 -0700

    Got the icicle chart transitions working.

--- public/js/icicle.js
+++ public/js/icicle.js
@@ -70,7 +70,7 @@ export async function icicle() {
 
     text.append("tspan").text((d) => d.data.name);
 
-    let format = d3.format(",d");
+    const format = d3.format(",d");
     let tspan = text
         .append("tspan")
         .attr("fill-opacity", (d) => labelVisible(d) * 0.7)
@@ -88,42 +88,15 @@ export async function icicle() {
     let focus = root;
 
     function clicked(event, p) {
+        const old_focus_id = focus.data.id;
+
         if (focus.data.id == p.data.id)
         {
+            // Going up the tree is easy. We don't need to get any new data. Just transition.
             p = p.parent;
-        }
-        focus = focus === p ? (p = p.parent) : p;
+            focus = focus === p ? (p = p.parent) : p;
 
-        getData(p.data.id, num_layers-1).then(new_data => {
-
-            // Append new data to the old data structure
-            let dark = p;
-            const p_ids = []
-            while (dark.data.id != 0)
-            {
-                p_ids.push(dark.data.id);
-                dark = dark.parent;
-            }
-
-            let x = data;
-            p_ids.slice(1).reverse().forEach(p_id => {
-                let dark = x.child_ids.findIndex((element) => element == p_id);
-                x = x.children[dark];
-            });
-
-            let dark2 = x.child_ids.findIndex((element) => element == p_ids[0]);
-            x.children[dark2] = new_data;
-
-            // Recompute the hierarchy and partition with the new data structure
-            hierarchy = d3
-                .hierarchy(data)
-                .sum((d) => d.dsize)
-                .sort((a, b) => b.height - a.height || b.dsize - a.dsize);
-
-            root = d3
-                .partition()
-                .size([height, ((hierarchy.height + 1) * width) / num_layers])(hierarchy);
-
+            // Target the new focus
             root.each(
                 (d) =>
                     (d.target = {
@@ -133,37 +106,6 @@ export async function icicle() {
                         y1: d.y1 - p.y0,
                     }),
             );
-
-            svg.selectAll("g").remove();
-
-            cell = svg
-                .selectAll("g")
-                .data(root.descendants())
-                .join("g")
-                .attr("transform", (d) => `translate(${d.y0},${d.x0})`);
-
-            text = cell
-                .append("text")
-                .style("user-select", "none")
-                .attr("pointer-events", "none")
-                .attr("x", 4)
-                .attr("y", 13)
-                .attr("fill-opacity", (d) => +labelVisible(d));
-
-            rect = cell
-                .append("rect")
-                .attr("width", (d) => d.y1 - d.y0 - 1)
-                .attr("height", (d) => rectHeight(d))
-                .attr("fill-opacity", 0.6)
-                .attr("fill", (d) => {
-                    if (!d.depth) return "#ccc";
-                    while (d.depth > 1) d = d.parent;
-                    return color(d.data.name);
-                })
-                .style("cursor", "pointer")
-                .on("click", clicked);
-
-            text.append("tspan").text((d) => d.data.name);
 
             const t = cell
                 .transition()
@@ -176,7 +118,142 @@ export async function icicle() {
             rect.transition(t).attr("height", (d) => rectHeight(d.target));
             text.transition(t).attr("fill-opacity", (d) => +labelVisible(d.target));
             tspan.transition(t).attr("fill-opacity", (d) => labelVisible(d.target) * 0.7);
-        });
+        }
+        else
+        {
+            // Going down the tree is complicated. Get new data. Redraw the current graph and then transition.
+            focus = focus === p ? (p = p.parent) : p;
+            getData(p.data.id, num_layers-1).then(new_data => {
+
+                // Append new data to the old data structure
+                let dark = p;
+                const p_ids = []
+                while (dark.data.id != 0)
+                {
+                    p_ids.push(dark.data.id);
+                    dark = dark.parent;
+                }
+
+                let x = data;
+                p_ids.slice(1).reverse().forEach(p_id => {
+                    let dark = x.child_ids.findIndex((element) => element == p_id);
+                    x = x.children[dark];
+                });
+
+                let dark2 = x.child_ids.findIndex((element) => element == p_ids[0]);
+                x.children[dark2] = new_data;
+
+                // Recompute the hierarchy and partition with the new data structure
+                hierarchy = d3
+                    .hierarchy(data)
+                    .sum((d) => d.dsize)
+                    .sort((a, b) => b.height - a.height || b.dsize - a.dsize);
+
+                root = d3
+                    .partition()
+                    .size([height, ((hierarchy.height + 1) * width) / num_layers])(hierarchy);
+
+                let dark3 = root.descendants().find(d => d.data.id === old_focus_id);
+                const old_focus = {
+                    x0: dark3.x0,
+                    x1: dark3.x1,
+                    y0: dark3.y0,
+                    y1: dark3.y1,
+                };
+
+                // Target the old focus
+                root.each(
+                    (d) =>
+                        (d.target = {
+                            x0: ((d.x0 - old_focus.x0) / (old_focus.x1 - old_focus.x0)) * height,
+                            x1: ((d.x1 - old_focus.x0) / (old_focus.x1 - old_focus.x0)) * height,
+                            y0: d.y0 - old_focus.y0,
+                            y1: d.y1 - old_focus.y0,
+                        }),
+                );
+
+                svg.selectAll("g").remove();
+
+                cell = svg
+                    .selectAll("g")
+                    .data(root.descendants())
+                    .join("g")
+                    .attr("transform", (d) => `translate(${d.y0},${d.x0})`);
+
+                text = cell
+                    .append("text")
+                    .style("user-select", "none")
+                    .attr("pointer-events", "none")
+                    .attr("x", 4)
+                    .attr("y", 13)
+                    .attr("fill-opacity", (d) => +labelVisible(d));
+
+                rect = cell
+                    .append("rect")
+                    .attr("width", (d) => d.y1 - d.y0 - 1)
+                    .attr("height", (d) => rectHeight(d))
+                    .attr("fill-opacity", 0.6)
+                    .attr("fill", (d) => {
+                        if (!d.depth) return "#ccc";
+                        while (d.depth > 1) d = d.parent;
+                        return color(d.data.name);
+                    })
+                    .style("cursor", "pointer")
+                    .on("click", clicked);
+
+
+                text.append("tspan").text((d) => d.data.name);
+
+                tspan = text
+                    .append("tspan")
+                    .attr("fill-opacity", (d) => labelVisible(d) * 0.7)
+                    .text((d) => ` ${format(d.dsize)}`);
+
+                cell.append("title").text(
+                    (d) =>
+                        `${d
+                            .ancestors()
+                            .map((d) => d.data.name)
+                            .reverse()
+                            .join("/")}\n${format(d.dsize)}`,
+                );
+
+                const t0 = cell
+                    .transition()
+                    .duration(0)
+                    .attr(
+                        "transform",
+                        (d) => `translate(${d.target.y0},${d.target.x0})`,
+                    );
+
+                rect.transition(t0).attr("height", (d) => rectHeight(d.target));
+                text.transition(t0).attr("fill-opacity", (d) => +labelVisible(d.target));
+                tspan.transition(t0).attr("fill-opacity", (d) => labelVisible(d.target) * 0.7);
+
+                // Target the new focus
+                root.each(
+                    (d) =>
+                        (d.target = {
+                            x0: ((d.x0 - p.x0) / (p.x1 - p.x0)) * height,
+                            x1: ((d.x1 - p.x0) / (p.x1 - p.x0)) * height,
+                            y0: d.y0 - p.y0,
+                            y1: d.y1 - p.y0,
+                        }),
+                );
+
+                const t = cell
+                    .transition()
+                    .duration(750)
+                    .attr(
+                        "transform",
+                        (d) => `translate(${d.target.y0},${d.target.x0})`,
+                    );
+
+                rect.transition(t).attr("height", (d) => rectHeight(d.target));
+                text.transition(t).attr("fill-opacity", (d) => +labelVisible(d.target));
+                tspan.transition(t).attr("fill-opacity", (d) => labelVisible(d.target) * 0.7);
+            });
+        }
     }
 
     function rectHeight(d) {
