24ad3f4fd036d01fc3302775abfc3da5c8fab25a
leakec <leakec57@gmail.com>
2024-06-29 07:37:03 -0700

    Re-did this to work with the original json, so we don't need to convert to nested first.

--- database.py
+++ database.py
@@ -1,10 +1,9 @@
 import json
-import pandas as pd
 import psycopg2
 
 # Load JSON data into a DataFrame
-with open("new.json") as f:
-    data = json.load(f)
+with open("new2.json") as f:
+    data = json.load(f)[3]
 
 # Connect to PostgreSQL
 conn = psycopg2.connect(
@@ -17,12 +16,21 @@ conn = psycopg2.connect(
 cursor = conn.cursor()
 
 ID = 0
-def recurseData(node,parent_id=0):
+def recurseData(dir_or_file,parent_id=0):
     """Recurse through the data structure. Do the following:
     * Add an id field to each object.
     * Sum up dsize and asize based on children.
     * Add each node to the postgres database."""
     global ID
+
+    is_dir = isinstance(dir_or_file, list)
+
+    if is_dir:
+        # This is a directory. The directory information is the first element in the list
+        node = dir_or_file[0]
+    else:
+        # This is a file. We can just set the node equal to it.
+        node = dir_or_file
 
     # Set default values for this node
     node.setdefault("dsize", 0)
@@ -32,19 +40,23 @@ def recurseData(node,parent_id=0):
     node["child_ids"] = []
     ID += 1
 
-    # Move through all children first. This ensures
-    # the children will have an ID and dsize.
-    for child in node.get("children", []):
-        recurseData(child, parent_id=node["id"])
-        node["asize"] += child["asize"]
-        node["dsize"] += child["dsize"]
-        node["child_ids"].append(child["id"])
+    if is_dir and len(dir_or_file) > 1:
+        # Move through the children of the directory first. This ensures the children will have an ID and dsize.
+        for child in dir_or_file[1:]:
+            child = recurseData(child, parent_id=node["id"])
 
-    # Insert DataFrame to PostgreSQL
+            # Add child information to this node
+            node["asize"] += child["asize"]
+            node["dsize"] += child["dsize"]
+            node["child_ids"].append(child["id"])
+
+    # Insert node into PostgreSQL
     cursor.execute(
         "INSERT INTO db (id, name, dsize, asize, parent_id, child_ids) VALUES (%s, %s, %s, %s, %s, %s)",
         (node["id"], node["name"], node["dsize"], node["asize"], node["parent_id"], node["child_ids"])
     )
+
+    return node
 
 recurseData(data)
 
