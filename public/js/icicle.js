22a42b9b0b4746a7922ed153019262caf16487ed
leakc57@gmail.com <carl.leake@jpl.nasa.gov>
2024-06-30 16:32:41 -0700

    Moving to class so we can have different chart types.

--- public/js/icicle.js
+++ public/js/icicle.js
@@ -6,6 +6,11 @@ export class Chart{
     width = 1000;
     height = 750;
     data = null;
+    hierarchy = null;
+    root = null;
+    svg = null;
+    color = null;
+    focus = null;
 
     // For selecting the data type: dsize or asize
     data_type_select = document.createElement("Select");
@@ -15,6 +20,9 @@ export class Chart{
     // For selecting the chart type
     chart_type_select = document.createElement("Select");
     chart_type = "icicle";
+
+    // Current plot to draw
+    plot = null;
 
     constructor() {
         // For selecting the chart type
@@ -34,6 +42,7 @@ export class Chart{
         this.data_type_select.addEventListener("change", () => {
             this.data_type = this.data_type_select.value;
             this.data_type_h = this.data_type + "_h";
+            this.plot.redraw();
         });
         this.data_type_select.style.position = "absolute";
         this.data_type_select.style.top = '10px';
@@ -42,95 +51,118 @@ export class Chart{
 
     async init() {
         this.data = await this.getData(0, this.num_layers-1);
+        this.plot = new Icicle(this);
     }
 
     async getData(nodeId, length) {
         return fetch(`http://localhost:3000/node/${nodeId}/${length}`).then(x=>x.json()).then(data=>data['0']['get_child_as_json']);
     }
 
-    icicle() {
+    computeHierarcy() {
+        // Recompute the hierarchy and partition with the new data structure
+        this.hierarchy = d3.hierarchy(this.data)
 
-        // Helper function to set the rectangle height
-        const rectHeight = (d) => {
-            return d.x1 - d.x0 - Math.min(1, (d.x1 - d.x0) / 2);
-        }
+        // Use the pre-computed data_type as the nodes value.
+        this.hierarchy.each(d => d.value = d.data[this.data_type]);
 
-        // Helper function to decide whether a label is visible or not
-        const labelVisible = (d) => {
-            return d.y1 <= this.width && d.y0 >= 0 && d.x1 - d.x0 > 16;
-        }
-
-        // Create the SVG container.
-        const svg = d3
-            .create("svg")
-            .attr("viewBox", [0, 0, this.width, this.height])
-            .attr("width", this.width)
-            .attr("height", this.height)
-            .attr("style", "max-width: 100%; height: auto; font: 10px sans-serif; position: absolute; top: 50px;");
-
-        // Compute the layout.
-        let hierarchy = d3.hierarchy(this.data)
-
-        // Use the pre-computed data type as the nodes value.
-        hierarchy.each(d => d.value = d.data[this.data_type]);
-
-        let root = d3
-            .partition()
-            .size([this.height, ((hierarchy.height + 1) * this.width) / this.num_layers])(hierarchy);
-
-        const color = d3.scaleOrdinal(
+        // Compute colors based on the data
+        this.color = d3.scaleOrdinal(
             d3.quantize(d3.interpolateRainbow, this.data.children.length + 1),
         );
+    }
+}
+
+class Icicle {
+    chart = null;
+    cell = null;
+    rect = null;
+    text = null;
+    tspan = null;
+    old_focus_id = null;
+
+    constructor(chart)
+    {
+        this.chart = chart;
+
+        this.chart.svg = d3
+            .create("svg")
+            .attr("viewBox", [0, 0, this.chart.width, this.chart.height])
+            .attr("width", this.chart.width)
+            .attr("height", this.chart.height)
+            .attr("style", "max-width: 100%; height: auto; font: 10px sans-serif; position: absolute; top: 50px;");
+
+        this.draw();
+    }
+
+    // Helper function to set the rectangle height
+    rectHeight(d) {
+        return d.x1 - d.x0 - Math.min(1, (d.x1 - d.x0) / 2);
+    }
+
+    // Helper function to decide whether a label is visible or not
+    labelVisible(d) {
+        return d.y1 <= this.chart.width && d.y0 >= 0 && d.x1 - d.x0 > 16;
+    }
+
+    draw() {
+        // Recompute hierarcy for data
+        this.chart.computeHierarcy()
+
+        this.chart.root = d3
+            .partition()
+            .size([this.chart.height, ((this.chart.hierarchy.height + 1) * this.chart.width) / this.chart.num_layers])(this.chart.hierarchy);
 
         // Append cells.
-        let cell = svg
+        this.cell = this.chart.svg
             .selectAll("g")
-            .data(root.descendants())
+            .data(this.chart.root.descendants())
             .join("g")
             .attr("transform", (d) => `translate(${d.y0},${d.x0})`);
 
-        let text = cell
+        this.text = this.cell
             .append("text")
             .style("user-select", "none")
             .attr("pointer-events", "none")
             .attr("x", 4)
             .attr("y", 13)
-            .attr("fill-opacity", (d) => +labelVisible(d));
+            .attr("fill-opacity", (d) => +this.labelVisible(d));
 
-        let rect = cell
-            .append("rect")
+        this.rect = this.cell.append("rect")
             .attr("width", (d) => d.y1 - d.y0 - 1)
-            .attr("height", (d) => rectHeight(d))
+            .attr("height", (d) => this.rectHeight(d))
             .attr("fill-opacity", 0.6)
             .attr("fill", (d) => {
                 if (!d.depth) return "#ccc";
                 while (d.depth > 1) d = d.parent;
-                return color(d.data.name);
+                return this.chart.color(d.data.name);
             })
-            .style("cursor", "pointer");
+            .style("cursor", "pointer")
 
-        text.append("tspan").text((d) => d.data.name);
+        this.text.append("tspan").text((d) => d.data.name);
 
-        let tspan = text
-            .append("tspan")
-            .attr("fill-opacity", (d) => labelVisible(d) * 0.7)
-            .text((d) => " " + d.data[this.data_type_h]);
+        this.tspan = this.text.append("tspan")
+            .attr("fill-opacity", (d) => this.labelVisible(d) * 0.7)
+            .text((d) => " " + d.data[this.chart.data_type_h]);
 
-        cell.append("title").text(
+        this.cell.append("title").text(
             (d) =>
             `${d
                     .ancestors()
                     .map((d) => d.data.name)
                     .reverse()
-                    .join("/")}\n ${d.data[this.data_type_h]}`,
+                    .join("/")}\n ${d.data[this.chart.data_type_h]}`,
+
         );
 
-        let focus = root;
+        // TODO: Put this elsewhere
+        if (this.chart.focus == null)
+        {
+            this.chart.focus = this.chart.root;
+        }
 
         const clicked = (_, p) => {
-            const old_focus_id = focus.data.id;
-
-            if (focus.data.id == p.data.id)
+            this.old_focus_id = this.chart.focus.data.id;
+            if (this.chart.focus.data.id == p.data.id)
             {
                 if (p.parent == null)
                 {
@@ -140,36 +172,15 @@ export class Chart{
 
                 // Going up the tree is easy. We don't need to get any new data. Just transition.
                 p = p.parent;
-                focus = focus === p ? (p = p.parent) : p;
+                this.chart.focus = this.chart.focus === p ? (p = p.parent) : p;
 
-                // Target the new focus
-                root.each(
-                    (d) =>
-                    (d.target = {
-                        x0: ((d.x0 - p.x0) / (p.x1 - p.x0)) * this.height,
-                        x1: ((d.x1 - p.x0) / (p.x1 - p.x0)) * this.height,
-                        y0: d.y0 - p.y0,
-                        y1: d.y1 - p.y0,
-                    }),
-                );
-
-                const t = cell
-                    .transition()
-                    .duration(750)
-                    .attr(
-                        "transform",
-                        (d) => `translate(${d.target.y0},${d.target.x0})`,
-                    );
-
-                rect.transition(t).attr("height", (d) => rectHeight(d.target));
-                text.transition(t).attr("fill-opacity", (d) => +labelVisible(d.target));
-                tspan.transition(t).attr("fill-opacity", (d) => labelVisible(d.target) * 0.7);
+                this.transition(p, 750);
             }
             else
             {
                 // Going down the tree is complicated. Get new data. Redraw the current graph and then transition.
-                focus = focus === p ? (p = p.parent) : p;
-                this.getData(p.data.id, this.num_layers-1).then(new_data => {
+                this.chart.focus = this.chart.focus === p ? (p = p.parent) : p;
+                this.chart.getData(p.data.id, this.chart.num_layers-1).then(new_data => {
 
                     // Append new data to the old data structure
                     let dark = p;
@@ -180,7 +191,7 @@ export class Chart{
                         dark = dark.parent;
                     }
 
-                    let x = this.data;
+                    let x = this.chart.data;
                     p_ids.slice(1).reverse().forEach(p_id => {
                         let dark = x.child_ids.findIndex((element) => element == p_id);
                         x = x.children[dark];
@@ -189,17 +200,8 @@ export class Chart{
                     let dark2 = x.child_ids.findIndex((element) => element == p_ids[0]);
                     x.children[dark2] = new_data;
 
-                    // Recompute the hierarchy and partition with the new data structure
-                    hierarchy = d3.hierarchy(this.data)
-
-                    // Use the pre-computed data_type as the nodes value.
-                    hierarchy.each(d => d.value = d.data[this.data_type]);
-
-                    root = d3
-                        .partition()
-                        .size([this.height, ((hierarchy.height + 1) * this.width) / this.num_layers])(hierarchy);
-
-                    let dark3 = root.descendants().find(d => d.data.id === old_focus_id);
+                    // Redraw the chart at the old focus
+                    let dark3 = this.chart.root.descendants().find(d => d.data.id === this.old_focus_id);
                     const old_focus = {
                         x0: dark3.x0,
                         x1: dark3.x1,
@@ -207,105 +209,50 @@ export class Chart{
                         y1: dark3.y1,
                     };
 
-                    // Target the old focus
-                    root.each(
-                        (d) =>
-                        (d.target = {
-                            x0: ((d.x0 - old_focus.x0) / (old_focus.x1 - old_focus.x0)) * this.height,
-                            x1: ((d.x1 - old_focus.x0) / (old_focus.x1 - old_focus.x0)) * this.height,
-                            y0: d.y0 - old_focus.y0,
-                            y1: d.y1 - old_focus.y0,
-                        }),
-                    );
+                    this.redraw(old_focus);
 
-                    svg.selectAll("g").remove();
-
-                    cell = svg
-                        .selectAll("g")
-                        .data(root.descendants())
-                        .join("g")
-                        .attr("transform", (d) => `translate(${d.y0},${d.x0})`);
-
-                    text = cell
-                        .append("text")
-                        .style("user-select", "none")
-                        .attr("pointer-events", "none")
-                        .attr("x", 4)
-                        .attr("y", 13)
-                        .attr("fill-opacity", (d) => +labelVisible(d));
-
-                    rect = cell
-                        .append("rect")
-                        .attr("width", (d) => d.y1 - d.y0 - 1)
-                        .attr("height", (d) => rectHeight(d))
-                        .attr("fill-opacity", 0.6)
-                        .attr("fill", (d) => {
-                            if (!d.depth) return "#ccc";
-                            while (d.depth > 1) d = d.parent;
-                            return color(d.data.name);
-                        })
-                        .style("cursor", "pointer")
-                        .on("click", clicked);
-
-
-                    text.append("tspan").text((d) => d.data.name);
-
-                    tspan = text
-                        .append("tspan")
-                        .attr("fill-opacity", (d) => labelVisible(d) * 0.7)
-                        .text((d) => " " + d.data[this.data_type_h]);
-
-                    cell.append("title").text(
-                        (d) =>
-                        `${d
-                                .ancestors()
-                                .map((d) => d.data.name)
-                                .reverse()
-                                .join("/")}\n ${d.data[this.data_type_h]}`,
-                    );
-
-                    const t0 = cell
-                        .transition()
-                        .duration(0)
-                        .attr(
-                            "transform",
-                            (d) => `translate(${d.target.y0},${d.target.x0})`,
-                        );
-
-                    rect.transition(t0).attr("height", (d) => rectHeight(d.target));
-                    text.transition(t0).attr("fill-opacity", (d) => +labelVisible(d.target));
-                    tspan.transition(t0).attr("fill-opacity", (d) => labelVisible(d.target) * 0.7);
-
-                    // Target the new focus
-                    root.each(
-                        (d) =>
-                        (d.target = {
-                            x0: ((d.x0 - p.x0) / (p.x1 - p.x0)) * this.height,
-                            x1: ((d.x1 - p.x0) / (p.x1 - p.x0)) * this.height,
-                            y0: d.y0 - p.y0,
-                            y1: d.y1 - p.y0,
-                        }),
-                    );
-
-                    const t = cell
-                        .transition()
-                        .duration(750)
-                        .attr(
-                            "transform",
-                            (d) => `translate(${d.target.y0},${d.target.x0})`,
-                        );
-
-                    rect.transition(t).attr("height", (d) => rectHeight(d.target));
-                    text.transition(t).attr("fill-opacity", (d) => +labelVisible(d.target));
-                    tspan.transition(t).attr("fill-opacity", (d) => labelVisible(d.target) * 0.7);
+                    // Transition to the new spot
+                    this.transition(p, 750);
                 });
             }
-        }
+        };
 
-        // Set the callback function
-        rect.on("click", clicked);
-
-        return svg.node();
+        this.rect.on("click", clicked);
     }
-}
 
+    redraw(focus=null) {
+        if (focus == null)
+        {
+            focus = this.chart.focus;
+        }
+        this.chart.svg.selectAll("g").remove();
+        this.draw();
+        this.transition(focus, 0);
+    }
+
+    transition(p, time) {
+        // Target the new focus
+        this.chart.root.each(
+            (d) =>
+            (d.target = {
+                x0: ((d.x0 - p.x0) / (p.x1 - p.x0)) * this.chart.height,
+                x1: ((d.x1 - p.x0) / (p.x1 - p.x0)) * this.chart.height,
+                y0: d.y0 - p.y0,
+                y1: d.y1 - p.y0,
+            }),
+        );
+
+        const t = this.cell
+            .transition()
+            .duration(time)
+            .attr(
+                "transform",
+                (d) => `translate(${d.target.y0},${d.target.x0})`,
+            );
+
+        this.rect.transition(t).attr("height", (d) => this.rectHeight(d.target));
+        this.text.transition(t).attr("fill-opacity", (d) => +this.labelVisible(d.target));
+        this.tspan.transition(t).attr("fill-opacity", (d) => this.labelVisible(d.target) * 0.7);
+    }
+
+}
